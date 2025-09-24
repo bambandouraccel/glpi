040028612cb38cf3eaf8b2d6e2418673a223a83e
bambandouraccel <125573511+bambandouraccel@users.noreply.github.com>
2025-09-24 11:23:17 +0000

    Update init-volumes-directories.sh

--- init-volumes-directories.sh
+++ init-volumes-directories.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "[INIT] Initialisation des volumes et répertoires GLPI..."
 
-# Répertoires de configuration et de données
 GLPI_VAR_DIR="/var/glpi"
 GLPI_CONFIG_DIR="${GLPI_VAR_DIR}/config"
 GLPI_FILES_DIR="${GLPI_VAR_DIR}/files"
@@ -13,9 +12,8 @@ GLPI_PLUGIN_DIR="${GLPI_VAR_DIR}/plugins"
 # Création des répertoires si absents
 mkdir -p "$GLPI_CONFIG_DIR" "$GLPI_FILES_DIR" "$GLPI_LOG_DIR" "$GLPI_PLUGIN_DIR"
 
-# Droits (1001 = utilisateur non root)
-chown -R 1001:0 "$GLPI_VAR_DIR"
-chmod -R g+rw "$GLPI_VAR_DIR"
+# Adapter les droits pour OpenShift (pas de chown, juste chgrp + chmod)
+chgrp -R 0 "$GLPI_VAR_DIR"
+chmod -R g+rwX "$GLPI_VAR_DIR"
 
 echo "[INIT] Répertoires GLPI initialisés avec succès."
-
